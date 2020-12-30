cf3dac122605580ac734252e442e2d10fae8d2ae
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2020-12-30 15:59:23 +0100

    MyTwitter - start

---
+++ src/main/java/com/korolchuk1986/mytwitter/ServingWebContentApplication.java
@@ -0,0 +1,11 @@
+package com.korolchuk1986.mytwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+}
