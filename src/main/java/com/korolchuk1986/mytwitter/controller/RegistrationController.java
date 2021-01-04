2b57fbda06058676ff2c1bd012497ac0d1ec3bd3
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2021-01-05 00:54:28 +0100

    MyTwitter - add account activation

--- src/main/java/com/korolchuk1986/mytwitter/controller/RegistrationController.java
+++ src/main/java/com/korolchuk1986/mytwitter/controller/RegistrationController.java
@@ -1,20 +1,18 @@
 package com.korolchuk1986.mytwitter.controller;
 
-import com.korolchuk1986.mytwitter.domain.Role;
 import com.korolchuk1986.mytwitter.domain.User;
-import com.korolchuk1986.mytwitter.repo.UserRepo;
+import com.korolchuk1986.mytwitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.Collections;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration() {
@@ -23,14 +21,22 @@ public class RegistrationController {
     @PostMapping("/registration")
     public String addUser(User user, Model model) {
 
-        User userFromDb = userRepo.findByUsername(user.getUsername());
-        if (userFromDb != null) {
+        if (!userService.addUser(user)) {
             model.addAttribute("message", "User exists!");
             return "/registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
+
         return "redirect:/login";
+    }
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable String code, Model model) {
+        boolean isAcivated = userService.activateUser(code);
+
+        if (isAcivated) {
+            model.addAttribute("message", "User activated");
+        } else {
+            model.addAttribute("message", "User not activated");
+        }
+        return "/login";
     }
 }
