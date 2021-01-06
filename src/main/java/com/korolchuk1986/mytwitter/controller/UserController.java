fbdbd10cd139c8e095c01fc676a54598c5a67daf
Uladzimir Karalchuk <korolchuk1986@gmail.com>
2021-01-06 15:16:54 +0100

    MyTwitter - add edit profile

--- src/main/java/com/korolchuk1986/mytwitter/controller/UserController.java
+++ src/main/java/com/korolchuk1986/mytwitter/controller/UserController.java
@@ -2,32 +2,32 @@ package com.korolchuk1986.mytwitter.controller;
 
 import com.korolchuk1986.mytwitter.domain.Role;
 import com.korolchuk1986.mytwitter.domain.User;
-import com.korolchuk1986.mytwitter.repo.UserRepo;
+import com.korolchuk1986.mytwitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAnyAuthority('ADMIN')")
 public class UserController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        List<User> users = userRepo.findAll();
+        List<User> users = userService.findAll();
         model.addAttribute("users", users);
         return "userList";
     }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -35,29 +35,32 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
     @PostMapping
     public String saveUser (
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user
     ) {
-        user.setUsername(username);
-        Set<String> rolesSet = new HashSet<>();
-
-        for (Role role : Role.values()) {
-            rolesSet.add(role.name());
-        }
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if(rolesSet.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-
-        userRepo.save(user);
+        userService.save(user, username, form);
         return "redirect:/user";
     }
 
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("message", "Edit profile");
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(@AuthenticationPrincipal User user,
+                                @RequestParam String email,
+                                @RequestParam String password) {
+        userService.updateProfile(user, email, password);
+        return "redirect:/user/profile";
+    }
 
 }
--- src/main/java/com/korolchuk1986/mytwitter/service/UserService.java
+++ src/main/java/com/korolchuk1986/mytwitter/service/UserService.java
@@ -1,6 +1,5 @@
 package com.korolchuk1986.mytwitter.service;
 
-import antlr.StringUtils;
 import com.korolchuk1986.mytwitter.domain.Role;
 import com.korolchuk1986.mytwitter.domain.User;
 import com.korolchuk1986.mytwitter.repo.UserRepo;
@@ -9,9 +8,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -38,7 +37,14 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
 
-        if(user.getEmail() != null && !user.getEmail().isEmpty()) {
+        sendMessage(user);
+
+        return true;
+    }
+
+    private void sendMessage(User user) {
+
+        if(!StringUtils.isEmpty(user.getEmail())){
             String message = String.format(
                     "Hello, %s! \n" +
                             "Welcome to MyTwitter. Please, visit my link http://localhost:8080/activate/%s",
@@ -47,8 +53,6 @@ public class UserService implements UserDetailsService {
             );
             mailSender.sendEmail(user.getEmail(), "Activation code", message);
         }
-
-        return true;
     }
 
     public boolean activateUser(String code) {
@@ -62,6 +66,54 @@ public class UserService implements UserDetailsService {
         userRepo.save(user);
 
         return true;
+
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+
+    public void save(User user, String username, Map<String, String> form) {
+
+        user.setUsername(username);
+        Set<String> rolesSet = new HashSet<>();
+
+        for (Role role : Role.values()) {
+            rolesSet.add(role.name());
+        }
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if(rolesSet.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+
+        userRepo.save(user);
+    }
+
+    public void updateProfile(User user, String email, String password) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) || (userEmail != null && !userEmail.equals(email));
+
+        if(isEmailChanged) {
+            user.setEmail(email);
+            if(!StringUtils.isEmpty(email)) {
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if(!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepo.save(user);
+
+        if(isEmailChanged) {
+            sendMessage(user);
+        }
 
     }
 }
